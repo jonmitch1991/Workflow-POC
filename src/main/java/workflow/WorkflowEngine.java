d4fa00ebc94c288e561bb2714d210c23c6f83059
Jonathan Mitchell <jonathan.mitchell@atos.net>
2017-01-23 09:58:54 +0000

    Don't use reflection to find execute Method. Also, Steps for each Task are defeined in the WorkflowEngine constructor.

--- src/main/java/workflow/WorkflowEngine.java
+++ src/main/java/workflow/WorkflowEngine.java
@@ -1,35 +1,60 @@
 package main.java.workflow;
 
+import main.java.workflow.steps.RejectIPR;
+import main.java.workflow.steps.SubmitIPR;
+
 import javax.enterprise.context.ApplicationScoped;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @ApplicationScoped
 public class WorkflowEngine {
 
+    private Map<Task, List<String>> stepsByTask;
+
+    public WorkflowEngine() {
+        // This map would be populated from an XML file which detailed the Step's for each Task.
+        stepsByTask = new HashMap<>();
+        stepsByTask.put(Task.CREATE_IPR, new ArrayList<>());
+        stepsByTask.get(Task.CREATE_IPR).add("main.java.workflow.steps.ValidateIPR");
+        stepsByTask.get(Task.CREATE_IPR).add("main.java.workflow.steps.SaveIPR");
+        stepsByTask.get(Task.CREATE_IPR).add("main.java.workflow.steps.NotifyUsers");
+
+        stepsByTask.put(Task.SUBMIT_IPR, new ArrayList<>());
+        stepsByTask.get(Task.SUBMIT_IPR).add("main.java.workflow.steps.ValidateIPR");
+        stepsByTask.get(Task.SUBMIT_IPR).add("main.java.workflow.steps.SubmitIPR");
+        stepsByTask.get(Task.SUBMIT_IPR).add("main.java.workflow.steps.NotifyUsers");
+
+        stepsByTask.put(Task.APPROVE_IPR, new ArrayList<>());
+        stepsByTask.get(Task.APPROVE_IPR).add("main.java.workflow.steps.ApproveIPR");
+        stepsByTask.get(Task.APPROVE_IPR).add("main.java.workflow.steps.NotifyUsers");
+
+        stepsByTask.put(Task.REJECT_IPR, new ArrayList<>());
+        stepsByTask.get(Task.REJECT_IPR).add("main.java.workflow.steps.RejectIPR");
+        stepsByTask.get(Task.REJECT_IPR).add("main.java.workflow.steps.NotifyUsers");
+    }
+
     public String process(Map<String, Object> params) {
         Task task = (Task) params.get("task");
 
-        // Skipped finding Steps via XML for the time being and have hardcoded them into the findSteps method
-        List<String> steps = findSteps(task);
+        /*// Skipped finding Steps via XML for the time being and have hardcoded them into the findSteps method
+        List<String> steps = findSteps(task);*/
+        List<String> steps = stepsByTask.get(task);
         StringBuilder sb = new StringBuilder();
 
         for (String stepName : steps) {
             try {
                 Class<?> stepClass = Class.forName(stepName);
-                Method execute = stepClass.getDeclaredMethod("execute", java.util.Map.class);
-                String result = (String) execute.invoke(stepClass.newInstance(), params);
+                Step step = (Step) stepClass.newInstance();
+                String result = step.execute(params);
                 sb.append(result + " ");
             } catch (ClassNotFoundException e) {
                 System.out.println("Class not found " + stepName);
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
             } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             } catch (InstantiationException e) {
                 e.printStackTrace();
@@ -39,7 +64,7 @@ public class WorkflowEngine {
         return sb.toString();
     }
 
-    private List<String> findSteps(Task task) {
+    /*private List<String> findSteps(Task task) {
         List<String> results = new ArrayList<>();
         switch (task) {
             case CREATE_IPR:
@@ -64,5 +89,5 @@ public class WorkflowEngine {
                 break;
         }
         return results;
-    }
+    }*/
 }
